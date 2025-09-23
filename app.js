6760306f7bde2721e2bf591c125df2d4f42eeb02
Karam Abbas <kma69362@gmail.com>
2025-09-23 15:25:02 -0700

    WIP

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const fs = require('fs')
 const path = require('path')
+const express = require('express')
 const app = express()
 const PORT = 5000
 
-app.use(express.static(path.join(__dirname)))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () =>{
-    console.log('Server is running on http://localhost:5000');
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+app.use((err, req, next) =>{
+    res.status(404).send("Sorry, we couldn't find that page.")
+})
+
+app.use((err, req, next) =>{
+    res.status(500).send("Something broke server-side.")
+})
--- public/script.js
+++ public/script.js
@@ -3,10 +3,20 @@ const toggle = document.getElementById('toggleInfo')
 
 toggle.addEventListener('click', function(e){
     e.preventDefault()
-    infoSection.classList.toggle('hidden')
     if(infoSection.classList.contains('hidden')){
-        toggle.textContent = 'More Information'
-    }else{
         toggle.textContent = 'Hide Information'
+        infoSection.classList.replace('hidden', 'show')
+    }else{
+        toggle.textContent = 'Show Information'
+        infoSection.classList.replace('show', 'hidden')
     }
 })
+
+// const email = document.getElementById('email')
+// const emailInput = email.value
+
+// if(emailInput || /^[^\s@]+@[^\s@]+\.[^\s@]+$/){
+//     return;
+// }else{
+
+// }
